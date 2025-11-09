06c9646c0afaca1436962e7bf861ae507de331da
Vaishnav <vaishnavsnair6@gmail.com>
2025-11-09 15:02:44 +0530

    updated crud operations with a sql server database instead of storing in memory.

---
+++ WebAPI/Migrations/20251109064542_book data added.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class bookdataadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Title", "YearPublished" },
+                values: new object[,]
+                {
+                    { 1, "J.R.R. Tolkien", "The Hobbit", 1937 },
+                    { 2, "George Orwell", "1984", 1949 },
+                    { 3, "Harper Lee", "To Kill a Mockingbird", 1960 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
